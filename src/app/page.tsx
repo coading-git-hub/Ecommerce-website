bf8660f1041ba75de810b671e3db4e70da80b054
KIRAN AHMED <162012113+coading-git-hub@users.noreply.github.com>
2025-01-04 00:53:18 +0500

    Ecommerce--Website

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Carosal from '../app/componant/carosal'
+import Products from './products/page'
+
+export default function Home() {
+  return (
+    <>
+      <Carosal />
+      <Products />
+    </>
+  );
+}
